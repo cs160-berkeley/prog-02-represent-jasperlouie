526c3e3fcfdfd90047cd869194a21e182f0ab06e
Jasper Louie <jlouie95@gmail.com>
2016-03-02 04:05:47 -0800

    initial commit

---
+++ App/mobile/src/main/java/com/example/jasper/represent/CongressActivity.java
@@ -0,0 +1,14 @@
+package com.example.jasper.represent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jasper on 3/2/2016.
+ */
+public class CongressActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congress);
+    };
+}
---
+++ App/mobile/src/main/java/com/example/jasper/represent/LocateActivity.java
@@ -0,0 +1,91 @@
+package com.example.jasper.represent;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LocateActivity extends AppCompatActivity {
+
+    // UI references.
+    private AutoCompleteTextView zipView;
+    private Button continueButton;
+    private CheckBox useLocCB;
+    private TextView locateError;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate);
+        // Set up the login form.
+        zipView = (AutoCompleteTextView) findViewById(R.id.zip);
+        // populateAutoComplete();
+        useLocCB = (CheckBox) findViewById(R.id.use_locCB);
+        locateError = (TextView) findViewById(R.id.locateErrorText);
+        continueButton = (Button) findViewById(R.id.contButton);
+        continueButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLocate();
+            }
+        });
+
+        //mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private void attemptLocate(){
+        //do nothing for now besides go to next screen
+        if(!isZipValid(zipView.getText().toString())){
+            locateError.setText("Please enter valid 5 digit zipcode.");
+        }else{
+            locateError.setText("");
+            int zip = 1;
+            Intent intent = new Intent(this, CongressActivity.class);
+            intent.putExtra("ZIP", zip);
+            startActivity(intent);
+        }
+
+    }
+
+    private boolean isZipValid(String zip) {
+        //TODO: Replace this with your own logic
+        return zip.length() == 5;
+    }
+
+
+}
+
